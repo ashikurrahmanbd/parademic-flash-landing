ab780cfb541188b1309ebe7a24d5c7f86f9d3bf5
Ashikur Rahman <proashik012@gmail.com>
2025-08-07 20:18:03 +0600

    Mobile responsive done

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+(function($){
+    $(document).ready(function(){
+        
+        $('#lightSlider').lightSlider({
+
+            gallery:true,
+
+            item: 1,
+
+            minSlide:1,
+
+            maxSlide:1,
+
+            auto:false,
+            
+            controls: true,
+
+        }); 
+
+
+    });
+})(jQuery);
